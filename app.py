12c924e2e0ee01440b99f1cc85385dd73545fc47
guimarczewski <65767113+guimarczewski@users.noreply.github.com>
2023-10-19 20:43:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ class GoogleCloudUploader:
             st.error("Error: Google Cloud credentials not loaded")
 
 
-class UploadFileTab:
+class UploadFileTab_gcs:
     def __init__(self, uploader):
         self.uploader = uploader
         uploaded_credentials = st.file_uploader("Upload JSON credentials file")
@@ -106,10 +106,67 @@ class UploadFileTab:
             if st.button("Upload"):
                 self.uploader.upload_file(bucket_name, uploaded_file)
 
-class UploadCSVTab:
+class UploadCSVTab_gcs:
     def __init__(self, uploader):
         self.uploader = uploader
         uploaded_credentials = st.file_uploader("Upload JSON credentials file")
+        bucket_name = st.text_input("Bucket Name")
+        uploaded_file = st.file_uploader("Upload CSV file")
+
+        if uploaded_credentials:
+            self.uploader.load_credentials(uploaded_credentials)
+
+        if uploaded_file:
+            error_type = self.validate_csv_file(uploaded_file)
+            if error_type is not True:
+                self.show_error_message(error_type)
+            else:
+                if st.button("Upload"):
+                    self.uploader.upload_file(bucket_name, uploaded_file)
+
+    def validate_csv_file(self, uploaded_file):
+        # Verifique se a extensão do arquivo é `.csv`.
+        if not uploaded_file.name.endswith(".csv"):
+            return "invalid_extension"
+
+        # Verifique se o arquivo contém as colunas `data`, `lat`, `lon`, `vehicle`.
+        try:
+            df = pd.read_csv(uploaded_file)
+            if not all(col in df.columns for col in ["data", "lat", "lon", "vehicle"]):
+                return "missing_columns"
+        except Exception as e:
+            pass
+
+        # Verifique se o arquivo contém mais de 10 linhas.
+        if len(df) <= 10:
+            return "too_few_rows"
+
+        return True
+
+    def show_error_message(self, error_type):
+        if error_type == "invalid_extension":
+            st.error("The file must be a CSV file.")
+        elif error_type == "missing_columns":
+            st.error("The CSV file must contain the following columns: data, lat, lon, vehicle.")
+        elif error_type == "too_few_rows":
+            st.error("The CSV file must contain at least 11 rows.")
+
+class UploadFileTab_aws:
+    def __init__(self, uploader):
+        self.uploader = uploader
+        bucket_name = st.text_input("Bucket Name")
+        uploaded_file = st.file_uploader("Upload any file")
+
+        if uploaded_credentials:
+            self.uploader.load_credentials(uploaded_credentials)
+
+        if uploaded_file:
+            if st.button("Upload"):
+                self.uploader.upload_file(bucket_name, uploaded_file)
+
+class UploadCSVTab_aws:
+    def __init__(self, uploader):
+        self.uploader = uploader
         bucket_name = st.text_input("Bucket Name")
         uploaded_file = st.file_uploader("Upload CSV file")
 
@@ -165,10 +222,14 @@ def main():
         
     selected_tab = st.sidebar.selectbox("Select a tab:", ["Upload File", "Upload CSV with validation"])
 
-    if selected_tab == "Upload File":
+    if selected_tab == "Upload File" and selected_tab_cloud == "Google Cloud Storage":
+        UploadFileTab_gcs(uploader)
+    elif selected_tab == "Upload CSV with validation" and selected_tab_cloud == "Google Cloud Storage":
+        UploadCSVTab_gcs(uploader)
+    elif selected_tab_aws == "Upload File" and selected_tab_cloud == "AWS S3":
         UploadFileTab(uploader)
-    elif selected_tab == "Upload CSV with validation":
-        UploadCSVTab(uploader)
+    elif selected_tab == "Upload CSV with validation"and selected_tab_cloud == "AWS S3":
+        UploadCSVTab_aws(uploader)
 
 
 if __name__ == "__main__":
